91d69ce2cf89cb9b76d0f6dcd43eeefbe0db45ed
Thibaut Hoerner <hoerner.t@gmail.com>
2023-06-26 09:08:52 +0200

    dockerCompv2

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@
 git pull
 
 # Construire l'image Docker
-docker build --no-cache -t loc-mns-front .
+docker compose -f docker-compose.yml up -d --build
 
-# Arreter le conteneur existant
-docker stop conteneur-loc-mns-front
+# # Arreter le conteneur existant
+# docker stop conteneur-loc-mns-front
 
-# Supprimer le conteneur existant
-docker rm conteneur-loc-mns-front
+# # Supprimer le conteneur existant
+# docker rm conteneur-loc-mns-front
 
-# Lancer un nouveau conteneur
-docker run -d --name=conteneur-loc-mns-front -p 4200:80 loc-mns-front
+# # Lancer un nouveau conteneur
+# docker run -d --name=conteneur-loc-mns-front -p 4200:80 loc-mns-front
